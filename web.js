4a245b1e14d044b936559b6cda92125e6163bd19
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2013-07-19 20:26:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var html = fs.readFileSync('index.html');
+
 
 app.get('/', function(request, response) {
-  response.writeHead(200, {
-     'Content-type': 'text/html; 
-      charset=utf-8'
-  }); 
-  response.end(html);
+  var buffer = new Buffer();
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
